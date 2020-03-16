27c28264efed8eeff66f56556426d7482925e310
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2020-03-16 16:05:42 -0300

    Techs include and delete

---
+++ src/components/TechList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: ['NodeJS', 'ReactJS', 'React Native'],
+  }
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ 
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+     });
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button 
+                onClick={ () => this.handleDelete(tech) } 
+                type="button">
+                Deletar
+              </button>
+            </li>
+          ))}
+        </ul>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange} 
+          value={this.state.newTech} 
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const soma = (a, b) => a+b;
+import React from 'react';
+import { render } from 'react-dom';
 
-alert(soma(5,5));
+import App from './App'
+
+render(<App />, document.getElementById('app'));
