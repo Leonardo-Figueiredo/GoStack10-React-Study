cd92e8e365579ace47cb3bf82b890e6ec51f849a
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2020-03-19 08:58:53 -0300

    All concepts r fixed.

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,47 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import TechItem from './TechItem';
 
 class TechList extends Component {
   state = {
-    newTech: '',
-    techs: ['NodeJS', 'ReactJS', 'React Native'],
+    newTech: "",
+    techs: []
+  };
+
+  // Executado sempre que o componente parecer em tela.
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+    
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado.
+  componentDidUpdate(_, prevState) {
+    //this.props, this.state
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+
+  }
+
+  // Executado quando o componente deixa de existir.
+  componentWillUnmount() {
+    
   }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
-  }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
 
-    this.setState({ 
+    this.setState({
       techs: [...this.state.techs, this.state.newTech],
-      newTech: '',
-     });
-  }
+      newTech: ""
+    });
+  };
 
-  handleDelete = (tech) => {
+  handleDelete = tech => {
     this.setState({ techs: this.state.techs.filter(t => t !== tech) });
-  }
+  };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => (
-            <li key={tech}>
-              {tech}
-              <button 
-                onClick={ () => this.handleDelete(tech) } 
-                type="button">
-                Deletar
-              </button>
-            </li>
-          ))}
-        </ul>
-        <input 
-          type="text" 
-          onChange={this.handleInputChange} 
-          value={this.state.newTech} 
+          {
+            this.state.techs.map(tech => (
+                <TechItem 
+                  key={tech} 
+                  tech={tech} 
+                  onDelete={() => this.handleDelete(tech)} 
+                />
+              ))}
+         </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
         />
         <button type="submit">Enviar</button>
       </form>
